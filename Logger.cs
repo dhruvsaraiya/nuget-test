6ceed1c376c216836b7ca968ce9904558a06c8e6
Dhruv Saraiya <dhruvsaraiya@github.com>
2021-12-23 19:16:08 +0530

    nuget

---
+++ Logger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace nuget_test{
+    public class Logger
+    {
+        static void Main() {
+             Console.WriteLine("Hellllo World!!!!!");
+         }
+        public void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
